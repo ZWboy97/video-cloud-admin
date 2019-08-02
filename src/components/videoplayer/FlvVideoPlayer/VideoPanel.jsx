a53b8417cf8dd1941e153ca7fb9ff5e97af373bf
NingNing.Wang <wangningning2016@bupt.edu.cn>
2019-08-02 16:30:00 +0800

    控制台界面

--- src/components/videoplayer/FlvVideoPlayer/VideoPanel.jsx
+++ src/components/videoplayer/FlvVideoPlayer/VideoPanel.jsx
@@ -10,22 +10,15 @@ class VideoPanel extends React.Component {
 
     render() {
         return (
-            <div className='video-control-box'>
-                <div className='video-box'>
-                    <div className='video-box-header'>
-                        <a className='video-name'>我的直播的名字</a>
-                        <img className="video-status" src={require('./icon_live.png')}></img>
-                    </div>
+                <div>
+                    
                     <FlvPlayer
                         className='flv-video-player'
                         url={'http://39.106.194.43:8090/live360/1458248448.flv'}
                         type="flv" />
-                    <div className='video-box-control'>
-                        控制按钮
-                    </div>
+                    
                 </div>
 
-            </div>
         )
     }
 }
