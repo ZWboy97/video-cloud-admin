fec55eef01c4b68dd9683bc673555b9e4d24b17c
NingNing.Wang <wangningning2016@bupt.edu.cn>
2019-08-12 17:44:00 +0800

    个人中心界面

--- src/components/usercenter/UserCenter.jsx
+++ src/components/usercenter/UserCenter.jsx
@@ -1,59 +1,223 @@
 import React, { Component } from 'react';
-import { Row, Col, Card, Button, Select, Avatar } from 'antd';
+import { Row, Col, Card, Button, message, Select, Avatar, Tag, Input, Form, Upload } from 'antd';
 import BreadcrumbCustom from '../BreadcrumbCustom';
-const { Option } = Select;
-const options = ['topLeft', 'topRight', 'bottomLeft', 'bottomRight'];
-const UserList = ['U', 'Lucy', 'Tom', 'Edward'];
-const colorList = ['#f56a00', '#7265e6', '#ffbf00', '#00a2ae'];
-
+import { connectAlita } from 'redux-alita';
+import { VCloudAPI } from '../../axios/api';
+import { getLocalStorage } from '../../utils/index';
+import { checkUserInfo } from '../../utils/UserUtils';
+import OssUploader from '../../utils/OssUploader';
+const { TextArea } = Input;
 
 class UserCenter extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            user: UserList[0],
-            color: colorList[0],
-        };
+        this.handleClick = this.handleClick.bind(this)
+        this.handleDesc = this.handleDesc.bind(this)
     }
 
-    changeUser = () => {
-        const index = UserList.indexOf(this.state.user);
-        this.setState({
-            user: index < UserList.length - 1 ? UserList[index + 1] : UserList[0],
-            color: index < colorList.length - 1 ? colorList[index + 1] : colorList[0],
+
+    componentDidMount() {
+        if (!checkUserInfo(this.props.history)) {//检查用户信息是否完整
+            return;
+        }
+        const user = getLocalStorage('user');
+        VCloudAPI.get("/user/" + user.aid + '/info/?is_com=' + true, {
+
+        }).then(response => {
+            if (response.status === 200) {
+                const { code = 0, data = {}, msg = {} } = response.data || {};
+                console.log(data)
+                if (code === 200) {
+                    // 向用户直播列表中添加一个记录
+                    message.success('成功获取用户信息')
+                    this.props.setAlitaState({
+                        stateName: 'user_info',
+                        data: data
+                    });
+
+                } else {
+                    message.error('获取配置失败!')
+                }
+            } else {
+                message.error('网络请求失败！')
+            }
+        }).catch(r => {
+        })
+    }
+    handleClick() {
+        
+        this.props.form.getFieldValue('email')
+        if (!checkUserInfo(this.props.history)) {//检查用户信息是否完整
+            return;
+        }
+        const user = getLocalStorage('user');
+        const { user_info = {} } = this.props.alitaState || {};
+        const userInfo = user_info.data || {}
+        const data = { 
+            "name": this.props.form.getFieldValue('name'),
+            "email": this.props.form.getFieldValue('email') ,
+            "avtar_url": userInfo.avtar_url,
+            "desc": userInfo.desc 
+
+        }
+        VCloudAPI.put("/user/" + user.aid + '/info/?is_com=' + true, {
+            ...data
+        }).then(response => {
+            if (response.status === 200) {
+                const { code = 0, data = {}, msg = {} } = response.data || {};
+                console.log(data)
+                if (code === 200) {
+                    // 向用户直播列表中添加一个记录
+                    message.success('成功获取用户信息')
+                    this.props.setAlitaState({
+                        stateName: 'user_info',
+                        data: data
+                    });
+
+                } else {
+                    message.error('获取配置失败!')
+                }
+            } else {
+                message.error('网络请求失败！')
+            }
+        }).catch(r => {
+        })
+    }
+    handleDesc(e) {
+        console.log(e.target.value)
+        const { user_info = {} } = this.props.alitaState || {};
+        const userInfo = user_info.data || {}
+        const data = { ...userInfo, "desc": e.target.value }
+        this.props.setAlitaState({
+            stateName: 'user_info',
+            data: data
         });
-    };
+
+    }
+    beforeUpload = file => {
+        // 配置STS Token， 之后需要从STS服务器去取
+        const options = {
+            config: {
+                region: 'oss-cn-beijing',
+                accessKeyId: "STS.NHzCXAWswMA6UhUVMWBkxcNP4",
+                accessKeySecret: "9WBs9RAL4ioo48xf17au4k4pxK57FsKkNhEGi672e95i",
+                stsToken: "CAISkwJ1q6Ft5B2yfSjIr4vPCOL1uqxW+oPdV07ksk0CTuRUjIv71jz2IH1KeHBsAugctvw/mmFX7PgSlqB6T55OSAmcNZIoS2+qPq3kMeT7oMWQweEuuv/MQBquaXPS2MvVfJ+OLrf0ceusbFbpjzJ6xaCAGxypQ12iN+/m6/Ngdc9FHHP7D1x8CcxROxFppeIDKHLVLozNCBPxhXfKB0ca3WgZgGhku6Ok2Z/euFiMzn+Ck7dL99mgfsT1MJE8Yc8jD+3YhrImKvDztwdL8AVP+atMi6hJxCzKpNn1ASMKvkvaaraPqoc3dF8nN/dgRf5e3/H4lOxlvOvIjJjwyBtLMuxTXj7WWIe62szAFfM14h+KhSJhUhqAAYlpjDY6CbeFKJT0L8T47wsPEm9QPtSUf5/1Mgyqk58HhyymtTPgcK3yX7Mp7qUOa6cu21UUMfLEKqY6ZlLy74NNpiY5x8iI9Py4LIYoRJNpuJJOl6QR/s5DSGiXDupTDkuyH8PgYVu1qmjRxAVQuFH5WWhUHQ4TynjZsbY/U63J",
+                bucket: 'pic-cloud-bupt',
+            },
+            dirname: '',
+            progress: this.progress,
+
+        }
+        // 创建Uploader
+        const upload = new OssUploader({
+            ...options,                 // 与文件无关的一些配置
+            file,                       // 待上传的文件
+        });
+        // 开始上传
+        upload.start(
+            res => {
+                console.log('OSS上传返回结果', res)
+                if (res.res.status === 200) {
+                    message.success('文件上传成功');
+                    const url = res.res.requestUrls[0];
+                    const { user_info = {} } = this.props.alitaState || {};
+                    const userInfo = user_info.data || {}
+                    const data = { ...userInfo, "avtar_url": url }
+                    this.props.setAlitaState({
+                        stateName: 'user_info',
+                        data: data
+                    });
+                    console.log('上传文件的返回URL为', url);
+                } else {
+                    message.error('文件上传失败');
+                }
+            },
+            error => {
+                message.error('文件上传失败');
+            }
+        );
+        return false;       // 在这里，我们自己通过OSS上传，所以返回false，拦截Upload自己的上传
+    }
+    progress = (percent) => {
+        console.log('上传进度:', parseInt(percent * 100));
+    }
 
     render() {
+        const { getFieldDecorator } = this.props.form;
+
+        const { user_info = {} } = this.props.alitaState || {};
+        const userInfo = user_info.data || {}
+       
+
+        const tagsFromServer = userInfo.auth||['Movies', 'Books', 'Music', 'Sports'];
 
         return (
             <div className="gutter-example button-demo">
                 <BreadcrumbCustom first="用户中心" />
-                <Row gutter={16}>
-                    <Col className="gutter-row" md={24}>
-                        <div className="gutter-box">
-                            <Card title='头像' bordered={false}>
-                                <Avatar style={{ backgroundColor: this.state.color, verticalAlign: 'middle' }} size="large">
-                                    {this.state.user}
-                                </Avatar>
-                                <Button
-                                    size="small"
-                                    style={{ marginLeft: 16, verticalAlign: 'middle' }}
-                                    onClick={this.changeUser}
-                                >
-                                    Change
-                                </Button>
-                            </Card>
-                        </div>
-                    </Col>
-                </Row>
-                <Row gutter={16}>
-                    <Col className="gutter-row" md={24}>
-                        <div className="gutter-box">
-                            <Card bordered={false}>
-                                <Button className='modify-button' type='primary'> 点击修改 </Button>
-                            </Card>
-                        </div>
+                <div>&nbsp;</div>
+                <div>&nbsp;</div>
+                <Row >
+                    <Col md={12} offset={6}>
+                        <Card bordered={false}>
+                            <Form className="form-style" labelCol={{ span: 6 }} wrapperCol={{ span: 14 }} >
+                                <Form.Item label="头像">
+                                    {getFieldDecorator('avtar_url', {
+                                    })(
+                                        <Row>
+                                            <Col span={8}>
+                                                <Upload
+                                                    beforeUpload={this.beforeUpload}
+                                                    showUploadList={false}
+                                                >
+                                                    <img src={userInfo.avtar_url} alt="avatar" style={{ width: '100%' }} />
+                                                </Upload>
+                                            </Col>
+                                        </Row>
+                                    )}
+                                </Form.Item>
+                                <Form.Item label="用户名">
+                                    {getFieldDecorator('name', {
+                                        initialValue: userInfo.name || 'lalala',
+
+                                    })(
+                                        <Input />
+                                    )}
+                                </Form.Item>
+
+                                <Form.Item label="邮箱">
+                                    {getFieldDecorator('email', {
+                                        initialValue: userInfo.email || {},
+                                        rules: [{ required: true, message: '请输入直播频道名称' }],
+                                    })(
+                                        <Input />
+                                    )}
+                                </Form.Item>
+                                <Form.Item label="权限">
+                                    <div>{tagsFromServer.map(tag => (
+                                        <Tag
+                                            key={tag}
+                                            color={"blue"}
+                                        >
+                                            {tag}
+                                        </Tag>
+                                    ))}</div>
+                                </Form.Item>
+
+                                <Form.Item label="备注">
+
+                                    {getFieldDecorator('desc', {
+
+                                    })(
+                                        <TextArea value={userInfo.desc} rows={3} onChange={this.handleDesc} />
+                                    )}
+                                </Form.Item>
+                                <Row>
+                                    <Col span={2} offset={10}>
+                                        <Button type="primary" onClick={this.handleClick}>保存修改</Button>
+                                    </Col>
+                                </Row>
+                            </Form>
+                        </Card>
                     </Col>
                 </Row>
             </div>
@@ -61,4 +225,4 @@ class UserCenter extends Component {
     }
 }
 
-export default UserCenter;
+export default connectAlita()(Form.create()(UserCenter));
