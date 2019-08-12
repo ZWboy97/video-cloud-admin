5ec0179a0c0bd51ddbb349842c7d9fe610a4d43f
NingNing.Wang <wangningning2016@bupt.edu.cn>
2019-08-12 11:57:41 +0800

    解决冲突

--- src/components/live/mylives/LiveTable.jsx
+++ src/components/live/mylives/LiveTable.jsx
@@ -84,7 +84,7 @@ class LiveTable extends React.Component {
                         <Divider type="vertical" />
                         <a className="live-link" href="javascript:;" onClick={(e) => this.handleControl(e, record)}>控制台</a>
                         <Divider type="vertical" />
-                        <a className="live-link" href="/director" onClick={(e) => this.handleDirector(e, record)}>导播台</a>
+                        <a className="live-link" href="javascript:;" onClick={(e) => this.handleDirector(e, record)}>导播台</a>
 
                     </div>
             },
@@ -108,7 +108,7 @@ class LiveTable extends React.Component {
             data: {
                 liveData: record
             }
-        }) 
+        })   
     }
 
     handleControl(e, record) {
@@ -123,7 +123,7 @@ class LiveTable extends React.Component {
     }
     handleDirector(e, record) {
         e.preventDefault();
-     //   this.props.history.push('/director');
+        this.props.history.push('/director');
         this.props.setAlitaState({
             stateName: 'live_control_page',
             data: {
