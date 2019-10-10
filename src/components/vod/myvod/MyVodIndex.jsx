4d958f59d424d422b0358becdae534efcc445d53
JiaChang.Li <1769264507@qq.com>
2019-10-10 16:17:30 +0800

    ADD: 添加点播基础设置的路由和框架

--- src/components/vod/myvod/MyVodIndex.jsx
+++ src/components/vod/myvod/MyVodIndex.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import MyVodPage from './vodlist/MyVodPage';
+import VodSetting from './vodsetting/VodSetting';
 
 /**
  * Page 页面， 由Router包裹
@@ -10,6 +11,7 @@ export default () => (
     <Switch>
         <Route exact path="/app/myvod/" render={() => (<Redirect to="/app/myvod/vodlist" />)} />
         <Route exact path="/app/myvod/vodlist/" component={MyVodPage} />
+        <Route exact path="/app/myvod/vodsetting/:rid" component={VodSetting} />
     </Switch>
 
 )
---
+++ src/components/vod/myvod/vodsetting/BasicVodSetting.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style.less';
+
+class BasicVodSetting extends React.Component {
+
+
+    render() {
+        return (
+            <div>
+                在这里编写基础设置界面
+            </div>
+        )
+    }
+}
+
+export default BasicVodSetting;
