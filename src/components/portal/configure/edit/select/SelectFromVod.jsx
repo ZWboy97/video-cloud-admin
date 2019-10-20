e7ead65fbd5b63d0ba65dd7e118117715fb9bb8b
JiaChang.Li <1769264507@qq.com>
2019-10-21 00:05:37 +0800

    ADD: 添加live选择,vod选择组件

--- src/components/portal/configure/edit/select/SelectFromVod.jsx
+++ src/components/portal/configure/edit/select/SelectFromVod.jsx
@@ -1,22 +1,12 @@
 import React from 'react';
 import { connectAlita } from 'redux-alita';
-import { List, message, Tag, Row, Col, Icon, Divider } from 'antd';
 import { VCloudAPI } from 'myaxios/api';
 import { getLocalStorage } from 'myutils/index';
 import { checkUserInfo } from 'myutils/UserUtils';
-import { withRouter } from 'react-router-dom';
-import './style.less';
+import { List, message, Tag, Row, Col, Divider } from 'antd';
 
+class SelectFromVod extends React.Component {
 
-// const IconText = ({ type, text }) => (
-//     <span>
-//         <Icon type={type} style={{ marginRight: 8 }} />
-//         {text}
-//     </span>
-// );
-
-//测试的数据
-class VodList extends React.Component {
     componentDidMount() {
         if (!checkUserInfo(this.props.history)) {//检查用户信息是否完整
             return;
@@ -30,7 +20,6 @@ class VodList extends React.Component {
                 console.log(data)
                 if (code === 200) {
                     // 向用户直播列表中添加一个记录
-                    message.success('成功获取点播列表')
                     this.props.setAlitaState({
                         stateName: 'vod_list_content',
                         data: {
@@ -50,15 +39,9 @@ class VodList extends React.Component {
 
 
     render() {
-
         const { vod_list_content = {} } = this.props.alitaState || {};
         const { data = {} } = vod_list_content || {}
-        console.log('data', data)
         const { videoInfo = [] } = data || {}
-
-
-        console.log('videoinfo', videoInfo)
-
         return (
             <Row>
                 <Col span={20} offset={2}>
@@ -69,7 +52,7 @@ class VodList extends React.Component {
                             onChange: page => {
                                 console.log(page);
                             },
-                            pageSize: 5,
+                            pageSize: 3,
                         }}
                         dataSource={videoInfo}
                         renderItem={item => (
@@ -78,7 +61,7 @@ class VodList extends React.Component {
                                     <Col span={4}>
                                         <img src={item.pic_url} alt="avatar" style={{ width: '100%', height: '90px' }} />
                                     </Col>
-                                    <Col span={13} offset={1}>
+                                    <Col span={16} offset={1}>
                                         <div className="top-show">
                                             <span className="video-name">{item.name}</span>
                                             <span className="video-time">{item.time}&nbsp;&nbsp;&nbsp;&nbsp;</span>
@@ -93,22 +76,13 @@ class VodList extends React.Component {
                                         </div>
                                         <div className="video-intro">{item.intro}</div>
                                     </Col>
-                                    <Col span={6}>
+                                    <Col span={2}>
                                         <div className="action">
                                             <a href="http://"
                                                 onClick={(e) => {
                                                     e.preventDefault();
                                                 }}
-                                            >删除</a>
-                                            <Divider type="vertical" />
-                                            <a href="http://"
-                                                onClick={(e) => {
-                                                    e.preventDefault();
-                                                    this.props.history.push(`/app/myvod/vodsetting/${item.rid}`)
-                                                }}
-                                            >设置</a>
-                                            <Divider type="vertical" />
-                                            <a href="http://">预览</a>
+                                            >选择</a>
                                         </div>
                                     </Col>
                                 </Row>
@@ -122,4 +96,5 @@ class VodList extends React.Component {
     }
 }
 
-export default withRouter(connectAlita()(VodList));
+
+export default connectAlita()(SelectFromVod);
