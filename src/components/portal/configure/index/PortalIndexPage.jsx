2085113083fb5b9937042bbe64e190312db3db7e
JiaChang.Li <1769264507@qq.com>
2019-10-21 21:49:59 +0800

    MOD: 完成门户管理首页数据调试

--- src/components/portal/configure/index/PortalIndexPage.jsx
+++ src/components/portal/configure/index/PortalIndexPage.jsx
@@ -4,11 +4,34 @@ import { message, Button, Card } from 'antd';
 import { withRouter } from 'react-router-dom';
 import QRCode from 'qrcode.react';
 import copyToBoard from 'copy-to-clipboard';
+import { getLocalStorage } from 'myutils/index';
+import { checkUserInfo } from 'myutils/UserUtils';
+import { VCloudAPI } from 'myaxios/api.js';
 import './style.less';
 
 class IndexPage extends React.Component {
 
+    state = {
+        day_visit: 0,
+        day_volume: 0,
+        portal_url: "",
+        total_visit: 0,
+        total_volume: 0
+    }
 
+    componentWillMount() {
+        if (!checkUserInfo(this.props.history)) {
+            return;
+        }
+        var user = getLocalStorage('user');
+        VCloudAPI.get(`/wportal/${user.cid}/info/`).then(response => {
+            if (response.data.code === 200 && response.data.data) {
+                this.setState(response.data.data)
+            } else {
+                message.error('请求数据错误');
+            }
+        });
+    }
 
     entryPortalEditPage = (e) => {
         this.props.history.push('/app/portal/configure/edit/');
@@ -26,15 +49,15 @@ class IndexPage extends React.Component {
                             headStyle={{ textAlign: "center" }}
                             style={{ width: 300 }}
                         >
-                            <div className="statist-number">30</div>
-                            <div className="total-number">总访问用户:2</div>
+                            <div className="statist-number">{this.state.day_visit}</div>
+                            <div className="total-number">总访问用户:{this.state.total_visit}</div>
                         </Card>
                         <Card title="今日访问量(PV)"
                             headStyle={{ textAlign: "center" }}
                             style={{ width: 300 }}
                         >
-                            <div className="statist-number">200</div>
-                            <div className="total-number">总访问量:10</div>
+                            <div className="statist-number">{this.state.day_volume}</div>
+                            <div className="total-number">总访问量:{this.state.total_volume}</div>
                         </Card>
                     </div>
 
@@ -47,14 +70,14 @@ class IndexPage extends React.Component {
                         </div>
                         <div className="link-container">
                             <QRCode className="qr-code" size={70}
-                                value={JSON.stringify("http://site.jackchance.cn")} />
+                                value={this.state.portal_url} />
                             <div className="link-operations">
                                 <a href="http://" onClick={(e) => {
                                     e.preventDefault();
-                                    copyToBoard('http://site.jackchance.cn');
+                                    copyToBoard(this.state.portal_url);
                                     message.success("复制成功");
                                 }}>复制链接</a>
-                                <a href="http://site.jackchance.cn" target="_blank">电脑端打开</a>
+                                <a href={this.state.portal_url} target="_blank">电脑端打开</a>
 
                             </div>
                         </div>
