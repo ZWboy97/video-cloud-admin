0b8b244d7998d7fee3791da5df405bdff1f75a76
JiaChang.Li <1769264507@qq.com>
2019-10-23 10:53:14 +0800

    MOD: 优化start.js脚本

--- scripts/start.js
+++ scripts/start.js
@@ -1,46 +1,44 @@
 'use strict';
 
-// Do this as the first thing so that any code reading it knows the right env.
+// 设置开发环境环境变量
 process.env.BABEL_ENV = 'development';
 process.env.NODE_ENV = 'development';
 
-// Makes the script crash on unhandled rejections instead of silently
-// ignoring them. In the future, promise rejections that are not handled will
-// terminate the Node.js process with a non-zero exit code.
+// 处理unhandle的错误，直接抛异常
 process.on('unhandledRejection', err => {
   throw err;
 });
 
-// Ensure environment variables are read.
+// 读取开发者配置的环境变量
 require('../config/env');
 
-
-const fs = require('fs');
-const chalk = require('chalk');
-const webpack = require('webpack');
-const WebpackDevServer = require('webpack-dev-server');
-const clearConsole = require('react-dev-utils/clearConsole');
-const checkRequiredFiles = require('react-dev-utils/checkRequiredFiles');
+// 加载一系列的依赖
+const fs = require('fs'); // 文件IO
+const chalk = require('chalk'); // 命令行颜色
+const webpack = require('webpack'); // webpack
+const WebpackDevServer = require('webpack-dev-server'); // webpack提供的dev-server
+const clearConsole = require('react-dev-utils/clearConsole'); // 清空命令行工具
+const checkRequiredFiles = require('react-dev-utils/checkRequiredFiles'); // 文件检查工具
 const {
   choosePort,
   createCompiler,
   prepareProxy,
   prepareUrls,
-} = require('react-dev-utils/WebpackDevServerUtils');
-const openBrowser = require('react-dev-utils/openBrowser');
-const paths = require('../config/paths');
-const config = require('../config/webpack.config.dev');
-const createDevServerConfig = require('../config/webpackDevServer.config');
+} = require('react-dev-utils/WebpackDevServerUtils'); // dev-server工具
+const openBrowser = require('react-dev-utils/openBrowser'); // 启动浏览器的工具
+const paths = require('../config/paths'); // 重新加载各个路径
+const config = require('../config/webpack.config.dev'); // 加载dev环境的webpack配置
+const createDevServerConfig = require('../config/webpackDevServer.config'); // 加载webpack-dev-server的配置
 
-const useYarn = fs.existsSync(paths.yarnLockFile);
+const useYarn = fs.existsSync(paths.yarnLockFile); // 通过是否存在yarn-lock文件判断是否使用yarn
 const isInteractive = process.stdout.isTTY;
 
-// Warn and crash if required files are missing
+// 判断两个重要的入口文件是否存在（index.html和index.js）
 if (!checkRequiredFiles([paths.appHtml, paths.appIndexJs])) {
   process.exit(1);
 }
 
-// Tools like Cloud9 rely on this.
+// 设置默认端口和host，可以从环境变量中读取，默认为3000端口
 const DEFAULT_PORT = parseInt(process.env.PORT, 10) || 3000;
 const HOST = process.env.HOST || '0.0.0.0';
 
@@ -61,18 +59,15 @@ if (process.env.HOST) {
   console.log();
 }
 
-// We require that you explictly set browsers and do not fall back to
-// browserslist defaults.
+// 启动webdevserver，然后在浏览器中打开前端页面
 const { checkBrowsers } = require('react-dev-utils/browsersHelper');
 checkBrowsers(paths.appPath, isInteractive)
   .then(() => {
-    // We attempt to use the default port but if it is busy, we offer the user to
-    // run on a different port. `choosePort()` Promise resolves to the next free port.
+    // 检测端口是否可用，选择一个可用端口
     return choosePort(HOST, DEFAULT_PORT);
   })
   .then(port => {
     if (port == null) {
-      // We have not found a port.
       return;
     }
     const protocol = process.env.HTTPS === 'true' ? 'https' : 'http';
@@ -100,7 +95,7 @@ checkBrowsers(paths.appPath, isInteractive)
       console.log(chalk.cyan('Starting the development server...\n'));
       openBrowser(urls.localUrlForBrowser);
     });
-
+    // 检测程序终止（Ctrl+C）和程序结束信号,检测到该信号则直接推出
     ['SIGINT', 'SIGTERM'].forEach(function (sig) {
       process.on(sig, function () {
         devServer.close();
